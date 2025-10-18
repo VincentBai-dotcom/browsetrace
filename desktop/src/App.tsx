bf891fe49ffa61177108de6bba370e7cebc16c5e
VincentBai-dotcom <butcheryyy@gmail.com>
2025-10-17 21:36:52 -0700

    init react

---
+++ desktop/src/App.tsx
@@ -0,0 +1,12 @@
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>Welcome to your Electron application with React.</p>
+    </div>
+  );
+}
+
+export default App;
